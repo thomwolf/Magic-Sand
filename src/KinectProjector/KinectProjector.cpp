b02183e00320e96aaf2814a012ea26c70a59aae0
Sijoma <simonschulte1991@gmail.com>
2017-07-24 17:01:30 +0200

    Kinectprojector interface now at bottom left position

--- src/KinectProjector/KinectProjector.cpp
+++ src/KinectProjector/KinectProjector.cpp
@@ -873,7 +873,7 @@ ofVec2f KinectProjector::gradientAtKinectCoord(float x, float y){
 
 void KinectProjector::setupGui(){
     // instantiate and position the gui //
-    gui = new ofxDatGui( ofxDatGuiAnchor::TOP_RIGHT );
+    gui = new ofxDatGui( ofxDatGuiAnchor::BOTTOM_LEFT);
     gui->addFRM();
     gui->addBreak();
     gui->addSlider("Tilt X", -30, 30, 0);
