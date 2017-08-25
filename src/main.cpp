d4a8afe357e1bb419f4912925d601db808fc75ff
Rasmus R. Paulsen <rapa@dtu.dk>
2017-08-25 23:54:59 +0200

    Updated version to alpha 1.5.1.1

--- src/main.cpp
+++ src/main.cpp
@@ -23,7 +23,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 #include "ofMain.h"
 #include "ofApp.h"
 
-const std::string MagicSandVersion = "1.5.1.0";
+const std::string MagicSandVersion = "1.5.1.1";
 
 bool setFirstWindowDimensions(ofGLFWWindowSettings& settings) {
 	// Check screens size and location
