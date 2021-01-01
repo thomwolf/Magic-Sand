706aed71b510e810ebb811ef8a29feb40fa0fbdf
Sriram Murali <sriram87@gmail.com>
2021-01-01 12:52:58 -0800

    Sriram - Capture the correct display output to the projector

--- src/main.cpp
+++ src/main.cpp
@@ -25,72 +25,70 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 
 const std::string MagicSandVersion = "1.5.4.2";
 
-bool setWindowDimensions(ofGLFWWindowSettings& settings, int windowsNum) {
-	int count;
-	GLFWmonitor** monitors = glfwGetMonitors(&count);
-	cout << "Number of screens found: " << count << endl;
-	if (count > windowsNum) {
-		int xM; int yM;
-		glfwGetMonitorPos(monitors[windowsNum], &xM, &yM); // We take the first monitor
-		const GLFWvidmode * desktopMode = glfwGetVideoMode(monitors[windowsNum]);
+ofGLFWWindowSettings setWindowDimensions(GLFWmonitor* monitor, bool mainWindow)
+{
+	// Create main screen
+	ofGLFWWindowSettings settings;
 
-        cout << "Monitor " << windowsNum << " size: " << desktopMode->width << "x" << desktopMode->height << endl;
-		
-		if (windowsNum == 0)
-		{
-			// Make main window almost full screen - but just a bit of space around to be able to grab other windows
-			settings.width = desktopMode->width * 4.0 / 5.0;
-			settings.height = desktopMode->height * 4.0 / 5.0;
-		}
-		else
-		{
-			// Projector window full screen
-			settings.width = desktopMode->width;
-			settings.height = desktopMode->height;
-		}
+	const GLFWvidmode * desktopMode = glfwGetVideoMode(monitor);
+	settings.width = desktopMode->width;
+	settings.height = desktopMode->height;
 
-		settings.setPosition(ofVec2f(xM, yM));
+	int xM; int yM;
+	glfwGetMonitorPos(monitor, &xM, &yM);
+	settings.setPosition(ofVec2f(xM, yM));
 
-		return true;
+	if (mainWindow)
+	{
+		settings.width *= 4.0 / 5.0;
+		settings.height *= 4.0 / 5.0;
+		settings.resizable = true;
+		settings.decorated = true;
+		settings.title = "Magic-Sand " + MagicSandVersion;
 	}
-	else {
-		settings.width = 1600; // Default settings
-		settings.height = 800;
-		settings.setPosition(ofVec2f(0, 0));
-		return false;
+	else
+	{
+		settings.resizable = false;
+		settings.decorated = false;
 	}
-
+	return settings;
 }
 
 //========================================================================
 int main() {
+
 	ofGLFWWindowSettings settings;
-//	setFirstWindowDimensions(settings);
-	//settings.width = 1200;
- //	settings.height = 600;
-    settings.width = 1600; // Default settings
-    settings.height = 800;
-    settings.setPosition(ofVec2f(0, 0));
-	settings.resizable = true;
-	settings.decorated = true;
-	settings.title = "Magic-Sand " + MagicSandVersion;
-	shared_ptr<ofAppBaseWindow> mainWindow = ofCreateWindow(settings);
-    
-	setWindowDimensions(settings, 0);
-	mainWindow->setWindowPosition(ofGetScreenWidth() / 2 - settings.width / 2, ofGetScreenHeight() / 2 - settings.height / 2);
-    mainWindow->setWindowShape(settings.width, settings.height);
-    
-	setWindowDimensions(settings, 1);
-	settings.resizable = false;
-	settings.decorated = false;
-	settings.shareContextWith = mainWindow;
-	shared_ptr<ofAppBaseWindow> secondWindow = ofCreateWindow(settings);
-	secondWindow->setVerticalSync(false);
+	settings.visible = false;
+	shared_ptr<ofAppBaseWindow> dummyWindow = ofCreateWindow(settings);
+
+	int count;
+	GLFWmonitor** monitors = glfwGetMonitors(&count);
+	cout << "Number of screens found: " << count << endl;
+
+	if (count == 0) {
+		cerr << "No monitors found. Exiting" << endl;
+		return -1;
+	}
+
+	// Set the debug window on Screen 0
+	ofGLFWWindowSettings mainWindowSettings = setWindowDimensions(monitors[0], true);
+	shared_ptr<ofAppBaseWindow> mainWindow = ofCreateWindow(mainWindowSettings);
+
+	mainWindow->setWindowPosition(ofGetScreenWidth() / 2 - mainWindowSettings.width / 2, ofGetScreenHeight() / 2 - mainWindowSettings.height / 2);
+	mainWindow->setWindowShape(mainWindowSettings.width, mainWindowSettings.height);
+
+	// Set the kinect window on Screen n - 1
+	ofGLFWWindowSettings projectorWindowSettings = setWindowDimensions(monitors[count - 1], false);
+	projectorWindowSettings.shareContextWith = mainWindow;
+	shared_ptr<ofAppBaseWindow> projectorWindow = ofCreateWindow(projectorWindowSettings);
+	projectorWindow->setVerticalSync(false);
 
 	shared_ptr<ofApp> mainApp(new ofApp);
-	ofAddListener(secondWindow->events().draw, mainApp.get(), &ofApp::drawProjWindow);
-	mainApp->projWindow = secondWindow;
-		
+	ofAddListener(projectorWindow->events().draw, mainApp.get(), &ofApp::drawProjWindow);
+	mainApp->projWindow = projectorWindow;
+
 	ofRunApp(mainWindow, mainApp);
 	ofRunMainLoop();
+	getchar();
+	return 0;
 }
