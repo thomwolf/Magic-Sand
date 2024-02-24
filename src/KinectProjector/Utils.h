6ec12ef0e121031e2f7cb9a1dc937fcd1137a808
seanth <seanth@gmail.com>
2024-02-23 22:06:13 -0500

    Update Utils.h
    commenting out unused parts to clean up compiling warnings

--- src/KinectProjector/Utils.h
+++ src/KinectProjector/Utils.h
@@ -63,36 +63,40 @@ namespace ofxCSG
 	{
 		return (p2 - p1).cross( p0 - p1).normalize();
 	}
-	
-	static float areaOfTriangle(ofVec3f p0, ofVec3f p1, ofVec3f p2)
-	{
-		return (p2 - p1).cross(p0 - p1).length() * .5;
-	}
-	
-	static float areaOfTriangleSquared(ofVec3f p0, ofVec3f p1, ofVec3f p2)
-	{
-		return (p2 - p1).cross(p0 - p1).lengthSquared() * .5;
-	}
 
-	static float signedDistanceToPlane(ofVec3f point, ofVec3f planePos, ofVec3f planeNormal)
-	{
-		float dist = planeNormal.dot(point - planePos);
-		return dist;
-	}
+	// Unused. Cleaning to make warnings. STH 2024-0222		
+	// static float areaOfTriangle(ofVec3f p0, ofVec3f p1, ofVec3f p2)
+	// {
+	// 	return (p2 - p1).cross(p0 - p1).length() * .5;
+	// }
 	
-	//http://geomalgorithms.com/a04-_planes.html
-	static float distanceToPlane(ofVec3f point, ofVec3f planePos, ofVec3f planeNormal)
-	{
-		float sb, sn, sd;
+	// Unused. Cleaning to make warnings. STH 2024-0222
+	// static float areaOfTriangleSquared(ofVec3f p0, ofVec3f p1, ofVec3f p2)
+	// {
+	// 	return (p2 - p1).cross(p0 - p1).lengthSquared() * .5;
+	// }
+
+	// Unused. Cleaning to make warnings. STH 2024-0222
+	// static float signedDistanceToPlane(ofVec3f point, ofVec3f planePos, ofVec3f planeNormal)
+	// {
+	// 	float dist = planeNormal.dot(point - planePos);
+	// 	return dist;
+	// }
+	
+	// Unused. Cleaning to make warnings. STH 2024-0222
+	// //http://geomalgorithms.com/a04-_planes.html
+	// static float distanceToPlane(ofVec3f point, ofVec3f planePos, ofVec3f planeNormal)
+	// {
+	// 	float sb, sn, sd;
 		
-		sn = -( planeNormal.dot(point - planePos) );
-		sd = planeNormal.dot(planeNormal);
-		sb = sn / sd;
+	// 	sn = -( planeNormal.dot(point - planePos) );
+	// 	sd = planeNormal.dot(planeNormal);
+	// 	sb = sn / sd;
 		
-		ofVec3f B = point + sb * planeNormal;
+	// 	ofVec3f B = point + sb * planeNormal;
 		
-		return point.distance(B);
-	}
+	// 	return point.distance(B);
+	// }
 	
 	static float distanceToPlaneSigned(ofVec3f point, ofVec3f planePos, ofVec3f planeNormal)
 	{
@@ -100,12 +104,13 @@ namespace ofxCSG
 		return -( planeNormal.dot( point - planePos ) );
 		//	return -( doubleDot( planeNormal, point - planePos ) );
 	}
-	
-	static Classification classifyPointWithPlane( ofVec3f point, ofVec3f planeNormal, float w )
-	{
-		float t = planeNormal.dot( point ) - w;
-		return ( t < NEG_EPSILON ) ? BACK : (t > EPSILON) ? FRONT : SPANNING;
-	}
+
+	// Unused. Cleaning to make warnings. STH 2024-0222	
+	// static Classification classifyPointWithPlane( ofVec3f point, ofVec3f planeNormal, float w )
+	// {
+	// 	float t = planeNormal.dot( point ) - w;
+	// 	return ( t < NEG_EPSILON ) ? BACK : (t > EPSILON) ? FRONT : SPANNING;
+	// }
 	
 	static Classification classifyPointWithPlane( ofVec3f point, ofVec3f planePos, ofVec3f planeNormal)
 	{
@@ -150,30 +155,32 @@ namespace ofxCSG
 		
 		return true;
 	}
-	
-	static bool getBaryCentricCoords(ofVec3f p, ofVec3f t0, ofVec3f t1, ofVec3f t2, float &u, float &v)
-	{
-		float w;
-		return getBaryCentricCoords(p, t0, t1, t2, u, v, w);
-	}
+
+	// Unused. Cleaning to make warnings. STH 2024-0222
+	// static bool getBaryCentricCoords(ofVec3f p, ofVec3f t0, ofVec3f t1, ofVec3f t2, float &u, float &v)
+	// {
+	// 	float w;
+	// 	return getBaryCentricCoords(p, t0, t1, t2, u, v, w);
+	// }
     
     static ofVec4f getPlaneEquation(ofVec3f basePlanePos, ofVec3f basePlaneNormal){
         ofVec4f basePlaneEq = basePlaneNormal/basePlaneNormal.length(); // Vecteur normal au plan normalisé
         basePlaneEq.w=-basePlaneNormal.dot(basePlanePos);
         return basePlaneEq;
     }
-	
-	static ofVec3f closestPointOnLineSegment(ofVec3f p, ofVec3f l0, ofVec3f l1)
-	{
-		ofVec3f diff = p - l0;
-		ofVec3f dir = l1 - l0;
-		float u = diff.dot( dir ) / dir.dot( dir );
+
+	// Unused. Cleaning to make warnings. STH 2024-0222	
+	// static ofVec3f closestPointOnLineSegment(ofVec3f p, ofVec3f l0, ofVec3f l1)
+	// {
+	// 	ofVec3f diff = p - l0;
+	// 	ofVec3f dir = l1 - l0;
+	// 	float u = diff.dot( dir ) / dir.dot( dir );
 		
-		if ( u < 0. )	return l0;
-		else if( u > 1. )	return l1;
+	// 	if ( u < 0. )	return l0;
+	// 	else if( u > 1. )	return l1;
 		
-		return l0 + dir * u;
-	}
+	// 	return l0 + dir * u;
+	// } 
 	
 	
 	//http://paulbourke.net/geometry/pointlineplane/lineline.c
@@ -236,68 +243,71 @@ namespace ofxCSG
 		float u = getLineSegmentUValue( l0, l1, p );
 		return  u >= NEG_EPSILON && u <= ONE_PLUS_EPSILON;
 	}
+
+	// Unused. Cleaning to make warnings. STH 2024-0222	
+	// static bool intersectLineSegments(ofVec3f a0, ofVec3f a1, ofVec3f b0, ofVec3f b1, ofVec3f* intersection=NULL)
+	// {
+	// 	ofVec3f p;
+		
+	// 	LineLineIntersect(a0, a1, b0, b1, &p);
+		
+	// 	if( isPointInLineSegment(a0, a1, p) )
+	// 	{
+	// 		*intersection = p;
+	// 		return true;
+	// 	}
+		
+	// 	return false;
+	// }
 	
-	static bool intersectLineSegments(ofVec3f a0, ofVec3f a1, ofVec3f b0, ofVec3f b1, ofVec3f* intersection=NULL)
-	{
-		ofVec3f p;
+	// Unused. Cleaning to make warnings. STH 2024-0222		
+	// static bool splitLineSegmentWithPlane( ofVec3f l0, ofVec3f l1, ofVec3f planeNormal, float w, ofVec3f* intersection)
+	// {
+	// 	auto c0 = classifyPointWithPlane( l0, planeNormal, w);
+	// 	auto c1 = classifyPointWithPlane( l1, planeNormal, w);
 		
-		LineLineIntersect(a0, a1, b0, b1, &p);
-		
-		if( isPointInLineSegment(a0, a1, p) )
-		{
-			*intersection = p;
-			return true;
-		}
-		
-		return false;
-	}
-	
-	static bool splitLineSegmentWithPlane( ofVec3f l0, ofVec3f l1, ofVec3f planeNormal, float w, ofVec3f* intersection)
-	{
-		auto c0 = classifyPointWithPlane( l0, planeNormal, w);
-		auto c1 = classifyPointWithPlane( l1, planeNormal, w);
-		
-		if( c0 != c1 )
-		{
-			float k = (w - planeNormal.dot(l0)) / planeNormal.dot( l1 - l0 );
+	// 	if( c0 != c1 )
+	// 	{
+	// 		float k = (w - planeNormal.dot(l0)) / planeNormal.dot( l1 - l0 );
 			
-			*intersection = lerp( l0, l1, CLAMP(k, 0, 1) ); // the clamp fixed some errors where k > 1
+	// 		*intersection = lerp( l0, l1, CLAMP(k, 0, 1) ); // the clamp fixed some errors where k > 1
 			
-			return true;
-		}
+	// 		return true;
+	// 	}
 		
-		return false;
-	}
+	// 	return false;
+	// }
 	
-	static int intersectLineSegmentPlane(ofVec3f p0, ofVec3f p1, ofVec3f planePos, ofVec3f planeNormal, ofVec3f* intersection = NULL)
-	{
-		auto d0 = distanceToPlaneSigned( p0, planePos, planeNormal );
-		auto d1 = distanceToPlaneSigned( p1, planePos, planeNormal );
+	// Unused. Cleaning to make warnings. STH 2024-0222		
+	// static int intersectLineSegmentPlane(ofVec3f p0, ofVec3f p1, ofVec3f planePos, ofVec3f planeNormal, ofVec3f* intersection = NULL)
+	// {
+	// 	auto d0 = distanceToPlaneSigned( p0, planePos, planeNormal );
+	// 	auto d1 = distanceToPlaneSigned( p1, planePos, planeNormal );
 		
-		if( (d0 >= EPSILON && d1 >= EPSILON) || ( d0 <= NEG_EPSILON && d1 <= NEG_EPSILON ) )
-//		if( (d0 > 0 && d1 > 0) || ( d0 < 0 && d1 < 0 ) )
-		{
-			//no intersection
-			return 0;
-		}
-		if( d0 == 0 && d1 == 0 )
-		{
-			//it's coplanar
-			if( intersection != NULL )
-			{
-				*intersection = p0;
-			}
-			return 2;
-		}
+	// 	if( (d0 >= EPSILON && d1 >= EPSILON) || ( d0 <= NEG_EPSILON && d1 <= NEG_EPSILON ) )
+	// 	//if( (d0 > 0 && d1 > 0) || ( d0 < 0 && d1 < 0 ) )
+	// 	{
+	// 		//no intersection
+	// 		return 0;
+	// 	}
+	// 	if( d0 == 0 && d1 == 0 )
+	// 	{
+	// 		//it's coplanar
+	// 		if( intersection != NULL )
+	// 		{
+	// 			*intersection = p0;
+	// 		}
+	// 		return 2;
+	// 	}
 		
-		//it's a hit
-		if( intersection != NULL )
-		{
-			//lerp using the distance to plane values
-			*intersection = lerp( p0, p1, d0 / (d0 - d1) );
-		}
-		return 1;
-	}
+	// 	//it's a hit
+	// 	if( intersection != NULL )
+	// 	{
+	// 		//lerp using the distance to plane values
+	// 		*intersection = lerp( p0, p1, d0 / (d0 - d1) );
+	// 	}
+	// 	return 1;
+	// }
 	
 	
 	static bool isPointInTriangle(ofVec3f p, ofVec3f a, ofVec3f b, ofVec3f c, ofVec3f normal )
@@ -313,60 +323,63 @@ namespace ofxCSG
 		
 		return false;
 	}
-	
-	static bool isPointOnPlane( ofVec3f p, ofVec3f planeNormal, float w, float epsilon = EPSILON)
-	{
-		float t = planeNormal.dot(p) - w;
-		return abs(t) > epsilon;
-	}
-	
-	static bool isPointInTriangle(ofVec3f p, ofVec3f a, ofVec3f b, ofVec3f c, ofVec3f normal, float epsilon )
-	{
-		float u, v, w;
+
+	// Unused. Cleaning to make warnings. STH 2024-0222		
+	// static bool isPointOnPlane( ofVec3f p, ofVec3f planeNormal, float w, float epsilon = EPSILON)
+	// {
+	// 	float t = planeNormal.dot(p) - w;
+	// 	return abs(t) > epsilon;
+	// }
+
+	// Unused. Cleaning to make warnings. STH 2024-0222		
+	// static bool isPointInTriangle(ofVec3f p, ofVec3f a, ofVec3f b, ofVec3f c, ofVec3f normal, float epsilon )
+	// {
+	// 	float u, v, w;
 		
-		if( getBaryCentricCoords( p, a, b, c, u, v, w ) )
-		{
-			return u > epsilon && v > epsilon && w > epsilon;
-		}
+	// 	if( getBaryCentricCoords( p, a, b, c, u, v, w ) )
+	// 	{
+	// 		return u > epsilon && v > epsilon && w > epsilon;
+	// 	}
 		
-		return false;
-	}
+	// 	return false;
+	// }
 	
 	static bool isPointInTriangle(ofVec3f p, ofVec3f a, ofVec3f b, ofVec3f c)
 	{
 		return isPointInTriangle( p, a, b, c, normalFromPoints(a, b, c) );
 	}
 	
-	
-	//derived from Akira-Hayasaka's ofxRayTriangleIntersection
-	//	https://github.com/Akira-Hayasaka/ofxRayTriangleIntersection/blob/master/src/ofxRayTriangleIntersection.h
-	//	assume ray direction is normalized
-	static bool intersectRayTriangle(ofVec3f rayOrigin, ofVec3f rayDir, ofVec3f t0, ofVec3f t1, ofVec3f t2, ofVec3f* intersection=NULL)
-	{
-		ofVec3f normal = (t2 - t1).cross( t0 - t1).normalize();
-		float vn = rayDir.dot(normal);
+
+	// Unused. Cleaning to make warnings. STH 2024-0222		
+	// //derived from Akira-Hayasaka's ofxRayTriangleIntersection
+	// //	https://github.com/Akira-Hayasaka/ofxRayTriangleIntersection/blob/master/src/ofxRayTriangleIntersection.h
+	// //	assume ray direction is normalized
+	// static bool intersectRayTriangle(ofVec3f rayOrigin, ofVec3f rayDir, ofVec3f t0, ofVec3f t1, ofVec3f t2, ofVec3f* intersection=NULL)
+	// {
+	// 	ofVec3f normal = (t2 - t1).cross( t0 - t1).normalize();
+	// 	float vn = rayDir.dot(normal);
 		
-		ofVec3f diff = rayOrigin - t0;
-		float xpn = diff.dot(normal);
-		float distance = -xpn / vn;
+	// 	ofVec3f diff = rayOrigin - t0;
+	// 	float xpn = diff.dot(normal);
+	// 	float distance = -xpn / vn;
 		
-		if (distance < 0) return false; // behind ray origin. fail
+	// 	if (distance < 0) return false; // behind ray origin. fail
 		
-		ofVec3f hitPos = rayDir * distance + rayOrigin;
+	// 	ofVec3f hitPos = rayDir * distance + rayOrigin;
 		
-		if(isPointInTriangle(hitPos, t0, t1, t2))
-		{
-			//it's a hit
-			if(intersection!= NULL)
-			{
-				*intersection = hitPos;
-			}
-			return true;
-		}
+	// 	if(isPointInTriangle(hitPos, t0, t1, t2))
+	// 	{
+	// 		//it's a hit
+	// 		if(intersection!= NULL)
+	// 		{
+	// 			*intersection = hitPos;
+	// 		}
+	// 		return true;
+	// 	}
 		
-		//nada
-		return false;
-	}
+	// 	//nada
+	// 	return false;
+	// }
     
     // Compute plane equation from point cloud
     static ofVec4f plane_from_points(ofVec3f* points, int n) {
